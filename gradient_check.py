Add MSELoss gradient check + fix MSELoss.

gradient_check.py
@@ -55,7 +55,59 @@ def testSoftmaxCrossEntropyLoss():
     #print('grad', grad_x)
     norm_diff = np.linalg.norm(grad_x - num_grad_x) / np.linalg.norm(grad_x + num_grad_x)
     ok = 'GOOD' if norm_diff < 1e-8 else 'BAD'
-    print('SoftmaxCrossEntropyLoss: norm diff %f %s' % (norm_diff, ok))
+    print('%s SoftmaxCrossEntropyLoss: norm diff %f' % (ok, norm_diff))
+
+'''
+Tests the analytical gradient computed in the backward pass of the MSELoss layer
+against the numerical gradient
+'''
+def testMSELoss():
+    # create input layers
+    X = Input(5, 5)
+    L = Input(5, 5)
+    x = X.top.value
+    l = L.top.value
+    x[:] = np.zeros((5, 5))
+    l[:] = np.eye(5)
+
+    # create mse loss layer 
+    mse_loss = MSELoss(X, L)
+    mse_loss.loss.grad = 1.0
+
+    # get analytical gradient
+    mse_loss.forward()
+    mse_loss.backward()
+    grad_x = mse_loss.bottom.grad.copy()
+
+    # compute numerical gradient
+    perturb = np.zeros_like(x)
+    num_grad_x = np.zeros_like(x)
+
+    for i in range(x.shape[0]):
+        for j in range(l.shape[1]):
+            old_x = x[i][j]
+
+            x[i][j] = old_x + epsilon
+
+            mse_loss.forward()
+
+            num_grad_x[i][j] = mse_loss.loss.value
+
+            x[i][j] = old_x - epsilon
+
+            mse_loss.forward()
+
+            num_grad_x[i][j] -= mse_loss.loss.value
+            num_grad_x[i][j] /= 2.0 * epsilon
+
+            x[i][j] = old_x
+
+    #print('num grad', num_grad_x)
+    #print('grad', grad_x)
+    norm_diff = np.linalg.norm(grad_x - num_grad_x) / np.linalg.norm(grad_x + num_grad_x)
+    ok = 'GOOD' if norm_diff < 1e-8 else 'BAD'
+    print('%s MSELoss: norm diff %f' % (ok, norm_diff))
 
 testSoftmaxCrossEntropyLoss()
+testMSELoss()
 

layers.py
@@ -182,16 +182,13 @@ class MSELoss(Layer):
         self.labels     = labels_layer.top
         self.batch_size = self.bottom.value.shape[0]
         self.num_inputs = self.bottom.value.shape[1]
-        self.top        = Blob(np.zeros(self.batch_size))
-
-    def reset_gradient(self):
-        self.top.reset_gradient()
+        self.loss       = Blob(np.zeros(1))
 
     def forward(self):
-        self.top.value = np.mean(np.square(self.bottom.value - self.labels.value), axis=1)
+        self.loss.value = np.mean(np.square(self.bottom.value - self.labels.value))
 
     def backward(self):
-        self.bottom.grad += (2.0 / self.num_inputs) * (self.bottom.value - self.labels.value) * self.top.grad
+        self.bottom.grad += (2.0 / (self.num_inputs * self.batch_size)) * (self.bottom.value - self.labels.value) * self.loss.grad
 
 class CrossEntropyLoss(Layer):
     def __init__(self, prev_layer, labels_layer):