Add CrossEntropyLoss gradient check + fix CELoss.

gradient_check.py
@@ -29,7 +29,6 @@ def testSoftmaxCrossEntropyLoss():
     grad_x = softmax_loss.bottom.grad.copy()
 
     # compute numerical gradient
-    perturb = np.zeros_like(x)
     num_grad_x = np.zeros_like(x)
 
     for i in range(x.shape[0]):
@@ -70,7 +69,7 @@ def testMSELoss():
     x[:] = np.zeros((5, 5))
     l[:] = np.eye(5)
 
-    # create mse loss layer 
+    # create mse loss layer
     mse_loss = MSELoss(X, L)
     mse_loss.loss.grad = 1.0
 
@@ -80,7 +79,6 @@ def testMSELoss():
     grad_x = mse_loss.bottom.grad.copy()
 
     # compute numerical gradient
-    perturb = np.zeros_like(x)
     num_grad_x = np.zeros_like(x)
 
     for i in range(x.shape[0]):
@@ -108,6 +106,57 @@ def testMSELoss():
     ok = 'GOOD' if norm_diff < 1e-8 else 'BAD'
     print('%s MSELoss: norm diff %f' % (ok, norm_diff))
 
+'''
+Tests the analytical gradient computed in the backward pass of the CrossEntropyLoss layer
+against the numerical gradient
+'''
+def testCrossEntropyLoss():
+    # create input layers
+    X = Input(5, 5)
+    L = Input(5, 5)
+    x = X.top.value
+    l = L.top.value
+    x[:] = np.ones((5, 5))
+    l[:] = np.eye(5)
+
+    # create cross entropy loss layer
+    crossentropy_loss = CrossEntropyLoss(X, L)
+    crossentropy_loss.loss.grad = 1.0
+
+    # get analytical gradient
+    crossentropy_loss.forward()
+    crossentropy_loss.backward()
+    grad_x = crossentropy_loss.bottom.grad.copy()
+
+    # compute numerical gradient
+    num_grad_x = np.zeros_like(x)
+
+    for i in range(x.shape[0]):
+        for j in range(l.shape[1]):
+            old_x = x[i][j]
+
+            x[i][j] = old_x + epsilon
+
+            crossentropy_loss.forward()
+
+            num_grad_x[i][j] = crossentropy_loss.loss.value
+
+            x[i][j] = old_x - epsilon
+
+            crossentropy_loss.forward()
+
+            num_grad_x[i][j] -= crossentropy_loss.loss.value
+            num_grad_x[i][j] /= 2.0 * epsilon
+
+            x[i][j] = old_x
+
+    # print('num grad', num_grad_x)
+    # print('grad', grad_x)
+    norm_diff = np.linalg.norm(grad_x - num_grad_x) / np.linalg.norm(grad_x + num_grad_x)
+    ok = 'GOOD' if norm_diff < 1e-8 else 'BAD'
+    print('%s CrossEntropyLoss: norm diff %f' % (ok, norm_diff))
+
 testSoftmaxCrossEntropyLoss()
 testMSELoss()
+testCrossEntropyLoss()
 

layers.py
@@ -196,17 +196,14 @@ class CrossEntropyLoss(Layer):
         self.labels     = labels_layer.top
         self.batch_size = self.bottom.value.shape[0]
         self.num_inputs = self.bottom.value.shape[1]
-        self.top        = Blob(np.zeros(self.batch_size))
-
-    def reset_gradient(self):
-        self.top.reset_gradient()
+        self.loss       = Blob(np.zeros(1))
 
     def cross_entropy(self, i, l):
         return -np.sum(np.multiply(np.log(i), l), axis = 1)
 
     def forward(self):
-        self.top.value = np.mean(self.cross_entropy(self.bottom.value, self.labels.value))
+        self.loss.value = np.mean(self.cross_entropy(self.bottom.value, self.labels.value))
 
     def backward(self):
-        self.bottom.grad += - self.labels.value / self.bottom.value * self.top.grad
+        self.bottom.grad += - (self.labels.value / self.bottom.value) / self.batch_size * self.loss.grad
 